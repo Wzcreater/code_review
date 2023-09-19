9fd8fdb9b5c7dba30bc77b8a7cc5b21bc595b5a8
zhangchuanzhan <1571403473@qq.com>
2023-09-19 17:50:02 +0800

    项目初始化

---
+++ src/main/java/tphy/peis/Application.java
@@ -0,0 +1,14 @@
+package tphy.peis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.printf("********体检PEIS系统启动成功********");
+    }
+
+}
